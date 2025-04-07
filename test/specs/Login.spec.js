7a3465d77d9c92b6346902cab119b7af3f9bdbe7
Shivakumar Gundra <shivakumar.gundra@feuji.com>
2025-04-07 15:09:13 +0530

    checking git actions

--- test/specs/Login.spec.js
+++ test/specs/Login.spec.js
@@ -7,15 +7,15 @@ describe('Login page', function () {
     it('Login with valid cred', async () => {
 
         await browser.url('https://practicetestautomation.com/practice-test-login/')
-      
-       await login.login('student', 'Password123')
-await login.getTitle()
-await login.getMessage()
-await login.Logout()
-await browser.pause(3000)
-     await browser.pause(3000)
 
-    
+        await login.login('student', 'Password123')
+        await login.getTitle()
+        await login.getMessage()
+        await login.Logout()
+        await browser.pause(3000)
+      
+
+
     })
 
 
